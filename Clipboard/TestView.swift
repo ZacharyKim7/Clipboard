eb49c50e8bfa385572fccdd70d4df365125ce54d
MatijaBenko <matijabenko@yahoo.com>
2024-09-02 16:40:20 -0700

    Fixed connection for TestView file

--- Clipboard/TestView.swift
+++ Clipboard/TestView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct TestView: View {
     var body: some View {
-        Text("Test View")
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+}
+
+#Preview {
+    TestView()
 }
